3cdc451bf9f69ccd75f91db19463821cefbbf69d
Mr-Alex-O <alexolszewski1993@gmail.com>
2017-02-22 19:20:15 -0500

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -32,6 +32,7 @@ let UserSchema = new mongoose.Schema({
   }]
 });
 
+//instance method
 UserSchema.methods.toJSON = function () {
   let user = this;
   let userObject = user.toObject();
@@ -48,6 +49,24 @@ UserSchema.methods.generateAuthToken = function () {
 
   return user.save().then(() => {
     return token;
+  });
+};
+
+//model method
+UserSchema.statics.findByToken = function (token) {
+  let User = this;
+  let decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
   });
 };
 
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
+let {authenticate} = require('./middleware/authenticate');
 
 let app = express();
 const port = process.env.PORT;
@@ -96,7 +97,7 @@ app.patch('/todos/:id', (req, res) => {
   })
 });
 
-// POST /users
+// POST /users signup route
 app.post('/users', (req, res) => {
   let body = _.pick(req.body, ['email', 'password']);
   let user = new User(body);
@@ -108,6 +109,10 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   })
+});
+
+app.get('/users/me',authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
